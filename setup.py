a5a3df7169b17de2e69b5461a2c64d4eee98ff20
Devin Duval <devinduval09@gmail.com>
2022-01-21 15:03:05 -0800

    updated entrypoints

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ setup(
         version='0.0.1',
         author='Devin Duval',
         author_email='DevinDuval09@gmail.com',
-        package_dir={'':'.'},
-        packages=['MUD_game', 'MUD_game.client', 'MUD_game.server'],
+        package_dir={'':'.', '.':'.'},
+        packages=['MUD_game', 'MUD_game.client', 'MUD_game.server', 'MUD_game.server.python_classes'],
         package_data={"":['*.css', '*.js', '*.html', '*.tsx', '*.jsx']},
         include_package_data=True,
         install_requires=['pymongo'],
-        entry_points={'console_scripts': ['MUD_game=game_server:main']},
+        entry_points={'console_scripts': ['MUD_game=game_server:main',
+                                           'create_room=MUD_game.server_utils:create_room'
+                                           ]},
         )
