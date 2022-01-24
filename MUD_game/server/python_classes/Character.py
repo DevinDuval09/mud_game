5e3c7ea4705d825c3df43a2772cec841c066c122
Devin Duval <devinduval09@gmail.com>
2022-01-24 15:22:02 -0800

    added _calc_AC, _calc_stat_bonus, AC, equip, and fixed save

--- MUD_game/server/python_classes/Character.py
+++ MUD_game/server/python_classes/Character.py
@@ -1,9 +1,9 @@
-from math import ceil
+from math import ceil, floor
 from enum import Enum
 from .Items import Equipment, Item
 from .Skills import EquipmentSlots, EquipmentClasses, EquipmentTypes, Skills
 '''Base class for all characters'''
-stats = ["strength", "dexterity", "constitution", "intelligence", "wisdom", "charisma"]
+stats = ["strength", "dexterity", "constitution", "intelligence", "wisdom", "charisma", "AC"]
 class Character:
     @classmethod
     def createNew(cls, name):
@@ -39,20 +39,58 @@ class Character:
         self.wisdom = WIS
         self.charisma = CHA
         self.inventory = inventory
-        self.equipment = equipment
+        self.equipment = {}
+        for slot in EquipmentSlots:
+            if slot not in equipment.keys() or equipment[slot] is None:
+                self.equipment[slot] = None
+            else:
+                self.equipment[slot] = equipment[slot]
+        self.AC = self._calc_AC()
         self.hit_dice = hit_dice
         self.level = level
         self.proficiency_bonus = ceil(self.level / 4) + 1
         self.general_proficiencies = general_prof #list of enums from Skills.py
         self.specific_proficiencies = specific_prof
         self.skills = skills
+    
+    def _calc_stat_bonus(self, stat: str):
+        return floor((getattr(self, stat) - 10) / 2)
+    
+    def _calc_AC(self):
+        class ARMOR_TYPE(Enum):
+            UNARMORED = 0
+            LIGHT_ARMOR = 1
+            MEDIUM_ARMOR = 2
+            HEAVY_ARMOR = 3
+        armor = ARMOR_TYPE.UNARMORED
+        armor_score = 0
+        for _, item in self.equipment.items():
+            if "AC" in dir(item):
+                armor_score += getattr(item, "AC")
+            if not item:
+                continue
+            if item.equipment_class == EquipmentClasses.LIGHT_ARMOR:
+                armor = ARMOR_TYPE.LIGHT_ARMOR
+            elif item.equipment_class == EquipmentClasses.MEDIUM_ARMOR:
+                armor = ARMOR_TYPE.MEDIUM_ARMOR
+            elif item.equipment_class == EquipmentClasses.HEAVY_ARMOR:
+                armor = ARMOR_TYPE.HEAVY_ARMOR
+        if armor == ARMOR_TYPE.UNARMORED:
+            return armor_score + self._calc_stat_bonus("dexterity") + 10
+        if armor == ARMOR_TYPE.LIGHT_ARMOR:
+            return armor_score + self._calc_stat_bonus("dexterity")
+        if armor == ARMOR_TYPE.MEDIUM_ARMOR:
+            return armor_score + min(self._calc_stat_bonus("dexterity"), 2)
+        if armor == ARMOR_TYPE.HEAVY_ARMOR:
+            return armor_score
+            
 
     def save(self) -> dict:
         save_dict = {"name": self.name}
         equipment_dict = {}
-        for slot, item in self.equipment.values():
+        for slot, item in self.equipment.items():
             if item:
-                equipment_dict[slot.value] = item.id
+                equipment_dict[str(slot.value)] = item.number
         save_dict["equipment"] = equipment_dict
         save_dict["inventory"] = [item.id for item in self.inventory.items()]
         for stat in stats:
@@ -86,8 +124,24 @@ class Character:
     def open(self):
         pass
 
-    def equip(self):
-        pass
+    def equip(self, item: str) -> str:
+        if item not in self.inventory.keys():
+            return f"You do not have {item} in your inventory."
+        item_object = self.inventory.pop(item)
+        if self.equipment[item_object.slot] is not None:
+            return f"You have to remove {self.equipment[item_object.slot]} first."
+        for skill in item_object.skills:
+            self.skills.append(skill)
+        for prof in item_object.general_proficiencies:
+            self.general_proficiencies.append(prof)
+        for prof in item_object.specific_proficiencies:
+            self.specific_proficiencies.append(prof)
+        for stat in dir(item_object):
+            if stat in stats:
+                current_stat = getattr(self, stat)
+                setattr(self, stat, current_stat + getattr(item_object, stat))
+        self.equipment[item_object.slot] = item_object
+        
 
     def remove(self):
         pass
