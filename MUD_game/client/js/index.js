3831f76e265def5d336ab5019ed7540c9012a31d
Devin Duval <devinduval09@gmail.com>
2023-02-10 13:03:01 -0800

    moved index.js and added index.js

---
+++ MUD_game/client/js/index.js
@@ -0,0 +1,5 @@
+"use strict";
+
+//build a state manager to handle incoming/outgoing messages from server
+//server sends json describing what the page looks like
+//state manager decodes json and updates panels as necessary
