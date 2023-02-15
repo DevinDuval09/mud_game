fa93f3cc31ad16744264f17d525b110b458b0d15
Devin Duval <devinduval09@gmail.com>
2023-02-15 14:57:20 -0800

    added GameStateManager

---
+++ MUD_game/server/utils/GameStateManager.py
@@ -0,0 +1,32 @@
+from .mongo import mongo
+class GameStateManager:
+    _STATES = ["LOGGED_OUT", "ACTIVE", "CHARACTER_CREATION"]
+    def __init__(self):
+        self._users = {}
+    def change_state(self, user, state):
+        if state.upper() not in GameStateManager._STATES:
+            raise TypeError(f"{state} is not a valid state.")
+        self._users[user] = state
+    def users(self):
+        return self._users.keys()
+    def add_user(self, user):
+        if user in self._users.keys():
+            raise KeyError(f"{user} is already in GameStateManager")
+        self._users[user] = "LOGGED_OUT"
+    def get_status(self, user):
+        if user not in self._users.keys():
+            raise KeyError(f"{user} is not in GameStateManager")
+        return self._users[user]
+    def login(self, character, password):
+        #hash password
+        with mongo:
+            #get character and password and verify password
+            #if password matches, toggle status to Active
+            pass
+    def create_character(self, character, password, **kwargs):
+        #hash password
+        with mongo:
+            #verify that character name isn't taken
+            #create load character into database
+            #switch character status to active
+            pass
