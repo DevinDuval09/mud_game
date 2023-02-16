17ba9d74ffdf512364f87cb9f57dc1399388eb6d
Devin Duval <devinduval09@gmail.com>
2023-02-16 07:49:44 -0800

    added deleteChildren, populateList, and validateForm

--- MUD_game/client/js/character_creation.js
+++ MUD_game/client/js/character_creation.js
@@ -21,4 +21,48 @@ const populateValues = () => {
     }
 }
 
+const deleteChildren = (elementList) => {
+    while (elementList.children[0]) {
+        elementList.children[0].remove();
+    }
+}
+
+const populateList = (strings, listElement) => {
+    deleteChildren(listElement);
+    for(const string of strings) {
+        const li = document.createElement("li");
+        li.appendChild(document.createTextNode(string));
+        listElement.appendChild(li);
+    }
+}
+
+const validateForm = (evt) => {
+    evt.preventDefault();
+    evt.stopPropagation();
+    const form = evt.target;
+    const attributeArray = [];
+    const errorList = document.querySelector("#error > ul");
+    deleteChildren(errorList);
+    const errors = [];
+    if (document.getElementById("password").value !== document.getElementById("password2").value) {
+        errors.push("Passwords do not match.");
+    }
+    for (const attribute of form.querySelectorAll("select")) {
+        attributeArray.push(attribute.value);
+    }
+    attributeArray.sort();
+    for (let i = 1; i < attributeArray.length; i++) {
+        if (attributeArray[i] == attributeArray[i-1]) {
+            errors.push("Each attribute can only be selected once.")
+        }
+    }
+    if (errors.length > 0) {
+        populateList(errors, errorList);
+        return;
+    }
+    form.submit();
+
+}
+
 populateValues();
+document.querySelector("form").addEventListener("submit", validateForm);
