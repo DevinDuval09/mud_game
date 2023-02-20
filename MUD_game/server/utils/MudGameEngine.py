0de4c935166e1cd1a336b3eadd6f4fa1e08be17e
Devin Duval <devinduval09@gmail.com>
2023-02-19 20:48:42 -0800

    added character_classes property, and create_new_character

--- MUD_game/server/utils/MudGameEngine.py
+++ MUD_game/server/utils/MudGameEngine.py
@@ -9,6 +9,11 @@ from ..python_classes.Items import Item
 ObjectManager is here to track the state of active objects on the server.
 '''
 class MudGameEngine:
+    character_classes = {
+                        "fighter": Fighter,
+                        "rogue": Rogue,
+                        "wizard": Wizard
+                        }
 
     def __init__(self, database:DatabaseConnect):
         #characters logged into this instance
@@ -42,6 +47,34 @@ class MudGameEngine:
             #generate message to moving player
             #generate messages to other players
             pass
+
+    def create_new_character(self, player:dict):
+        player_class = MudGameEngine.character_classes[player["type"]]
+        cleaned_dict = {}
+        for key, val in player.items():
+            if key == "strength":
+                cleaned_dict["STR"] = int(val)
+            elif key == "dexterity":
+                cleaned_dict["DEX"] = int(val)
+            elif key == "constitution":
+                cleaned_dict["CON"] = int(val)
+            elif key == "wisdom":
+                cleaned_dict["WIS"] = int(val)
+            elif key == "intelligence":
+                cleaned_dict["INT"] = int(val)
+            elif key == "charisma":
+                cleaned_dict["CHA"] = int(val)
+            elif key == "password":
+                pass #passwords aren't attached to objects
+            else:
+                cleaned_dict[key] = val
+        new_character = player_class(**cleaned_dict)
+        new_character.level = 1
+        new_character.room = 1
+        self.active_characters.append(new_character)
+        if 0 not in self.active_rooms.keys():
+            self.active_rooms[0] = self.db_connect.get_room(0)
+        return new_character
 
 
 
