fca9f673ae57a1eb7e1960418904e135e52ba36f
Devin Duval <devinduval09@gmail.com>
2023-02-19 16:35:26 -0800

    removed create_character

--- MUD_game/server/utils/MudGameEngine.py
+++ MUD_game/server/utils/MudGameEngine.py
@@ -17,24 +17,6 @@ class MudGameEngine:
         self.active_rooms={}
         self.db_connect = database
 
-    def create_character(self, name:str, *args, char_class=None, **kwargs):
-        doc = None
-        new_character = None
-        with mongo:
-            doc = mongo.db["Players"].find_one({"name": name})
-        if(doc):
-            #pull a character from the server
-            new_character = create_character_fromId(name)
-        else:
-            #make a brand new character and save it to the db
-            class_dict = {"Fighter": Fighter, "Rogue": Rogue, "Wizard": Wizard}
-            if char_class in class_dict.keys():
-                new_character = class_dict[char_class].create(*args, **kwargs)
-                
-        if new_character:
-            self.active_characters.append(new_character)
-
-
     def create_item(self, number:int) -> Item:
         return create_item_fromId(number)
 
